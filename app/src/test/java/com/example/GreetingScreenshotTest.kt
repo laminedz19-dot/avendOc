df4b7074d2d8824ebe1964b3dab9193298ff2cd6
laminedz19-dot <3.13565214e+08+laminedz19-dot@users.noreply.github.com>
2026-09-12 18:01:39 +0000

    Use supported SDK for Robolectric tests

--- app/src/test/java/com/example/GreetingScreenshotTest.kt
+++ app/src/test/java/com/example/GreetingScreenshotTest.kt
@@ -15,7 +15,7 @@ import org.robolectric.annotation.GraphicsMode
 
 @RunWith(RobolectricTestRunner::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
-@Config(qualifiers = RobolectricDeviceQualifiers.Pixel8, sdk = [36])
+@Config(qualifiers = RobolectricDeviceQualifiers.Pixel8, sdk = [35])
 class GreetingScreenshotTest {
 
   @get:Rule val composeTestRule = createComposeRule()
@@ -27,4 +27,3 @@ class GreetingScreenshotTest {
     composeTestRule.onRoot().captureRoboImage(filePath = "src/test/screenshots/greeting.png")
   }
 }
-
